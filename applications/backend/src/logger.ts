094d451fdeb19f5ac8e3ce350e989096c068d7c0
David Fasching <davidfasching@mailbox.org>
2021-08-22 15:23:12 +0200

    Add logger

---
+++ applications/backend/src/logger.ts
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+import winston, { format, Logger } from 'winston'
+
+const createLogger = (label: string): Logger =>
+	winston.createLogger({
+		format: format.combine(
+			format.timestamp(),
+			format.label({ label }),
+			format.printf((info) => `${info.timestamp} - [${info.level}] - [${info.label}] - ${info.message}`)
+		),
+		level: process.env.LOG_LEVEL ?? 'info',
+		transports: new winston.transports.Console(),
+	})
+
+export const logger = createLogger('Web')
