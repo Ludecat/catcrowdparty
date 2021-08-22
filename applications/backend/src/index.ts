54d2cb3a8bc6a8c10f4d73a596a4cea27977907c
David Fasching <davidfasching@mailbox.org>
2021-08-22 15:42:21 +0200

    Implement server side of example event

--- applications/backend/src/index.ts
+++ applications/backend/src/index.ts
@@ -1,5 +1,5 @@
 import { createServer } from 'http'
-import { SHARED } from '@ccp/common'
+import { DESTROY_CHECKBOXES, SHARED } from '@ccp/common'
 import { logger } from './logger'
 import { Server } from 'socket.io'
 
@@ -10,5 +10,14 @@ const io = new Server(httpServer, {})
 
 io.on('connection', (socket) => {
 	logger.info(`new connection from ${socket.id}!`)
+	setTimeout(() => {
+		logger.info(`destroying..`)
+		socket.emit(DESTROY_CHECKBOXES)
+	}, 3000)
+
+	socket.on('disconnect', (reason) => {
+		logger.info(`socket ${socket.id} disconnected with reason: ${reason}`)
+	})
 })
+
 httpServer.listen(5000)
