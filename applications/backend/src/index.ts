157b6f65920b6d2f35e98a568bb3005b5212a704
David Fasching <davidfasching@mailbox.org>
2021-08-22 15:32:36 +0200

    Add socket.io

--- applications/backend/src/index.ts
+++ applications/backend/src/index.ts
@@ -1,4 +1,14 @@
+import { createServer } from 'http'
 import { SHARED } from '@ccp/common'
 import { logger } from './logger'
+import { Server } from 'socket.io'
 
 logger.info(`Hello World! ${SHARED}`)
+
+const httpServer = createServer()
+const io = new Server(httpServer, {})
+
+io.on('connection', (socket) => {
+	logger.info(`new connection from ${socket.id}!`)
+})
+httpServer.listen(5000)
