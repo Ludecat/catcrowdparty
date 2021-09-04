a1584cd344d34c725aad8c8e0beba96fa0d0f32c
Patrick Obermueller <patrick.obermueller@willhaben.at>
2021-09-04 17:20:58 +0200

    display connect and disconnect on controlpanel ui

--- applications/frontend/src/app/hooks/useSocket.tsx
+++ applications/frontend/src/app/hooks/useSocket.tsx
@@ -1,9 +1,12 @@
+import { useRouter } from 'next/dist/client/router'
 import { useEffect, useContext, useState } from 'react'
+import { toast } from 'react-toastify'
 import { SocketContext } from '../provider/SocketProvider'
 
 export const useSocket = () => {
 	const { socket } = useContext(SocketContext)
 	const [isConnected, setIsConnected] = useState(false)
+	const router = useRouter()
 
 	useEffect(() => {
 		const cleanup = () => {
@@ -11,14 +14,26 @@ export const useSocket = () => {
 				socket.disconnect()
 			}
 		}
-		socket?.on('connect', () => {
-			setIsConnected(true)
-		})
+
 		if (socket) {
 			return cleanup
 		}
 		return
 	}, [socket])
+
+	useEffect(() => {
+		socket?.on('disconnect', () => {
+			if (router.pathname.includes('controlpanel')) {
+				toast('Socket disconnected', { type: 'warning' })
+			}
+		})
+		socket?.on('connect', () => {
+			setIsConnected(true)
+			if (router.pathname.includes('controlpanel')) {
+				toast('Socket connected', { type: 'info' })
+			}
+		})
+	}, [socket, router])
 
 	return { socket, isConnected }
 }
