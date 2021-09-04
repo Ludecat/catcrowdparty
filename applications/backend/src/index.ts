5fa2ae8dfddf17d7166fe43ca3a4f06d6bf24caf
David Fasching <davidfasching@mailbox.org>
2021-09-04 22:37:39 +0200

    Introduce redux for state management

--- applications/backend/src/index.ts
+++ applications/backend/src/index.ts
@@ -23,6 +23,7 @@ import {
 } from '@ccp/common'
 import { logger } from './logger'
 import { Server } from 'socket.io'
+import { configureStore, createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 const httpServer = createServer()
 const io = new Server<CCPSocketEventsMap>(httpServer, {})
@@ -30,11 +31,11 @@ const io = new Server<CCPSocketEventsMap>(httpServer, {})
 io.on('connection', (socket) => {
 	logger.info(`new connection from ${socket.id}!`)
 
-	socket.on(CROWD_UPDATE, (crowdUpdate: Partial<CrowdState>) => updateAndEmit(updateCrowd, crowdUpdate))
+	socket.on(CROWD_UPDATE, (crowdUpdate) => store.dispatch(crowdSlice.actions.update(crowdUpdate)))
 	socket.on(AUDIO_INPUT_VALUE_UPDATE, (data: AudioInputValue) => {
 		logger.debug(`received AUDIO_INPUT_VALUE_UPDATE ${data.averageFrequencyPower}`)
 
-		if (state.crowd.mode === CrowdMode.manual) {
+		if (store.getState().crowd.mode === CrowdMode.manual) {
 			logger.debug(`declined AUDIO_INPUT_VALUE_UPDATE caused by state.crowd.mode set to: '${CrowdMode.manual}'.`)
 			return
 		}
@@ -42,105 +43,130 @@ io.on('connection', (socket) => {
 		const crowdStateUpdate: Partial<CrowdState> = {
 			intensity: data.averageFrequencyPower,
 		}
-		updateAndEmit(updateCrowd, crowdStateUpdate)
+		store.dispatch(crowdSlice.actions.update(crowdStateUpdate))
 	})
-	socket.on(MODERATOR_UPDATE, (moderatorUpdate: Partial<ModeratorState>) =>
-		updateAndEmit(updateModerator, moderatorUpdate)
-	)
-	socket.on(HOT_AIR_BALLON_UPDATE, (hotAirBallonUpdate: Partial<HotAirBallonState>) =>
-		updateAndEmit(updateHotAirBallon, hotAirBallonUpdate)
+	socket.on(MODERATOR_UPDATE, (moderatorUpdate) => store.dispatch(moderatorSlice.actions.update(moderatorUpdate)))
+	socket.on(HOT_AIR_BALLON_UPDATE, (hotAirBallonUpdate) =>
+		store.dispatch(hotAirBallonSlice.actions.update(hotAirBallonUpdate))
 	)
 	socket.on(HOT_AIR_BALLON_START, (data: HotAirBalloonVariation) => {
 		logger.info(`received HOT_AIR_BALLON_START`)
 		io.emit(HOT_AIR_BALLON_START, data)
 	})
-	socket.on(EMOTES_UPDATE, (emotesUpdate: Partial<EmotesState>) => updateAndEmit(updateEmotes, emotesUpdate))
-	socket.on(BUBBLES_UPDATE, (bubblesUpdate: Partial<BubblesState>) => updateAndEmit(updateBubbles, bubblesUpdate))
+	socket.on(EMOTES_UPDATE, (emotesUpdate) => store.dispatch(emotesSlice.actions.update(emotesUpdate)))
+	socket.on(BUBBLES_UPDATE, (bubblesUpdate) => store.dispatch(bubblesSlice.actions.update(bubblesUpdate)))
 	socket.on('disconnect', (reason) => {
 		logger.info(`socket ${socket.id} disconnected with reason: ${reason}`)
 	})
 
-	socket.emit(STATE_UPDATE, state)
+	store.subscribe(() => {
+		socket.emit(STATE_UPDATE, store.getState())
+		console.log(store.getState())
+	})
 
-	socket.on(REQUEST_STATE, () => socket.emit(STATE_UPDATE, state))
+	socket.emit(STATE_UPDATE, store.getState())
+
+	socket.on(REQUEST_STATE, () => socket.emit(STATE_UPDATE, store.getState()))
 })
 
 const port = process.env.PORT_BACKEND ?? 5000
 httpServer.listen(port)
 logger.info(`Backend ready on port ${port}`)
 
-let state: GlobalState = {
-	crowd: {
-		mode: CrowdMode.manual,
-		intensity: 0,
-		visibility: true,
-	},
-	moderator: {
-		message: '',
-		visibility: false,
-	},
-	hotAirballon: {
-		visibility: false,
-	},
-	emotes: {
-		visibility: false,
-	},
-	bubbles: {
-		visibility: false,
-	},
+const initialModeratorState: ModeratorState = {
+	message: '',
+	visibility: false,
 }
 
-const updateCrowd = (state: GlobalState, crowdUpdate: Partial<CrowdState>): GlobalState => {
-	return {
-		...state,
-		crowd: {
-			...state.crowd,
-			...crowdUpdate,
+const moderatorSlice = createSlice({
+	name: 'moderator',
+	initialState: initialModeratorState,
+	reducers: {
+		update: (state, action: PayloadAction<Partial<ModeratorState>>) => {
+			return {
+				...state,
+				...action.payload,
+			}
 		},
-	}
+	},
+})
+
+const initialCrowdState: CrowdState = {
+	mode: CrowdMode.manual,
+	intensity: 0,
+	visibility: true,
 }
 
-const updateModerator = (state: GlobalState, moderatorUpdate: Partial<ModeratorState>): GlobalState => {
-	return {
-		...state,
-		moderator: {
-			...state.moderator,
-			...moderatorUpdate,
+const crowdSlice = createSlice({
+	name: 'crowd',
+	initialState: initialCrowdState,
+	reducers: {
+		update: (state, action: PayloadAction<Partial<CrowdState>>) => {
+			return {
+				...state,
+				...action.payload,
+			}
 		},
-	}
+	},
+})
+
+const initialHotAirBallonState: HotAirBallonState = {
+	visibility: false,
 }
 
-const updateHotAirBallon = (state: GlobalState, hotAirBallonUpdate: Partial<HotAirBallonState>): GlobalState => {
-	return {
-		...state,
-		hotAirballon: {
-			...state.hotAirballon,
-			...hotAirBallonUpdate,
+const hotAirBallonSlice = createSlice({
+	name: 'hotAirBallon',
+	initialState: initialHotAirBallonState,
+	reducers: {
+		update: (state, action: PayloadAction<Partial<HotAirBallonState>>) => {
+			return {
+				...state,
+				...action.payload,
+			}
 		},
-	}
+	},
+})
+
+const initialBubblesState: BubblesState = {
+	visibility: false,
 }
 
-const updateEmotes = (state: GlobalState, emotesUpdate: Partial<EmotesState>): GlobalState => {
-	return {
-		...state,
-		emotes: {
-			...state.emotes,
-			...emotesUpdate,
+const bubblesSlice = createSlice({
+	name: 'bubbles',
+	initialState: initialBubblesState,
+	reducers: {
+		update: (state, action: PayloadAction<Partial<BubblesState>>) => {
+			return {
+				...state,
+				...action.payload,
+			}
 		},
-	}
+	},
+})
+
+const initialEmotesState: EmotesState = {
+	visibility: false,
 }
 
-const updateBubbles = (state: GlobalState, bubblesUpdate: Partial<BubblesState>): GlobalState => {
-	return {
-		...state,
-		bubbles: {
-			...state.bubbles,
-			...bubblesUpdate,
+const emotesSlice = createSlice({
+	name: 'bubbles',
+	initialState: initialEmotesState,
+	reducers: {
+		update: (state, action: PayloadAction<Partial<EmotesState>>) => {
+			return {
+				...state,
+				...action.payload,
+			}
 		},
-	}
-}
+	},
+})
 
-const updateAndEmit = <T>(fn: (state: GlobalState, update: T) => GlobalState, update: T) => {
-	state = fn(state, update)
-	io.emit(STATE_UPDATE, state)
-}
+const store = configureStore<GlobalState>({
+	reducer: {
+		moderator: moderatorSlice.reducer,
+		crowd: crowdSlice.reducer,
+		hotAirballon: hotAirBallonSlice.reducer,
+		bubbles: bubblesSlice.reducer,
+		emotes: emotesSlice.reducer,
+	},
+})
