a672bfb90fbe50f2b3e43a1e2dc9210f63d1effb
Patrick Obermueller <patrick.obermueller@willhaben.at>
2021-09-12 11:56:59 +0200

    remove console log artifact

--- applications/frontend/src/app/overlay/game/scenes/OverlayScene.tsx
+++ applications/frontend/src/app/overlay/game/scenes/OverlayScene.tsx
@@ -79,7 +79,6 @@ export class OverlayScene extends Phaser.Scene {
 		})
 
 		config.socket.on(NEW_EMOTE_MESSAGE_TRIGGER, (senderName, emoteUrls, state) => {
-			console.log(state.visibility)
 			const activeEmoteBubbles = this.getActiveGameObjectsByName<EmoteBubble>('emoteBubble')
 			if (activeEmoteBubbles.length > 0) return
 			if (state.visibility) {
