b6c38ff6b272daa75f266e2ceba3a78f5044ce2e
Patrick Obermueller <patrick.obermueller@willhaben.at>
2021-10-06 20:06:18 +0200

    add socket url to env variable

--- applications/frontend/src/app/provider/SocketProvider.tsx
+++ applications/frontend/src/app/provider/SocketProvider.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import { createContext, FunctionComponent, useState } from 'react'
 import { io, Socket } from 'socket.io-client'
-import { CCPSocketEventsMap, SOCKET_URL } from '@ccp/common/shared'
+import { CCPSocketEventsMap } from '@ccp/common/shared'
 
 export interface SockerContextState {
 	socket: Socket<CCPSocketEventsMap> | null
@@ -16,7 +16,7 @@ export const SocketProvider: FunctionComponent = ({ children }) => {
 	const [socket, setSocket] = useState<Socket<CCPSocketEventsMap> | null>(socketDefaultValue.socket)
 
 	useEffect(() => {
-		setSocket(io(SOCKET_URL, { transports: ['websocket', 'polling'] }))
+		setSocket(io(process.env.NEXT_PUBLIC_SOCKET_URL as string, { transports: ['websocket', 'polling'] }))
 	}, [])
 
 	return (
